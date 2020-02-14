c3f322e84a2b535d6f29a65b5a0415a85b0758ae
Harsh Maheshwari <harshmaheshwari135@gmail.com>
2020-02-14 14:14:19 +0530

    tested part5, part 4 still remaining

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -7,9 +7,9 @@ global points
 global vanish_line
 
 def get_point(event,x,y,flags,param):
-  global points
-  if event == cv2.EVENT_LBUTTONDBLCLK:
-    points.append([x,y])
+	global points
+	if event == cv2.EVENT_LBUTTONDBLCLK:
+		points.append([x,y])
 
 def imshow(windowName, image, numLines):
 	global points
@@ -84,8 +84,10 @@ def part2(image):
 	P2 = part1(img, 4)
 
 	point1 = np.cross(P2[0], P2[1])
-	point1 /= point1[2]
 	point2 = np.cross(P2[2], P2[3])
+	line = np.cross([point1[0], point1[1], 1], [point2[0], point2[1], 1])
+	vanish_line = line
+	point1 /= point1[2]
 	point2 /= point2[2]
 	
 	cv2.line(img, (int(point1[0]), int(point1[1])), (int(point2[0]),int(point2[1])), (0,255,0), 2)
@@ -93,8 +95,6 @@ def part2(image):
 	cv2.imshow('vanishing line', img)
 	cv2.waitKey(0)
 	
-	line = np.cross([point1[0], point1[1], 1], [point2[0], point2[1], 1])
-	vanish_line = line
 	return line
 
 
@@ -127,22 +127,26 @@ def part4(image):
 	img = image.copy()
 	p = [img.shape[1]//2, img.shape[0]//2, 1]
 
-	H = np.random.rand(2,3)
-	a = np.array([-p[1],(p[0]-(float(p[1])/float(p[0]))),1]).astype(np.float32).reshape(1,-1)
-
+	H = np.random.rand(2,3) * 1000
+	# H = np.array([[1,0,0],[0,1,0]])
+	a = np.array([-p[1], p[0]-(1.0/p[1]) ,1]).astype(np.float32).reshape(1,-1)
 	H = np.append(H, a, axis = 0)
+	print(H)
 	result_point = H @ np.transpose(p)
 
-	projected_a = result_point[1]
-	projected_b = -result_point[0]
+	projected_a = -result_point[1]
+	projected_b = result_point[0]
 	
 	for c in range(1,4):
-		l = np.transpose(np.linalg.inv(H)) @ np.array([projected_a, projected_b, c]).reshape(-1, 1)
+		l = np.transpose(H) @ np.array([projected_a, projected_b, c*1000]).reshape(-1, 1)
 		a = l[0]
 		b = l[1]
-		point1 = (-l[2] - a)/b
-		point2 = (-l[2] - (a*200))/b
-		cv2.line(img, (int(1), int(point1)), (int(200),int(point2)), (0,255,0), 2)
+		point1 = (-l[2])/a
+		print(l)
+		print(point1)
+		# print(p)
+		# point2 = (-l[2] - (a*200))/b
+		cv2.line(img, (int(p[0]), int(p[1])), (int(point1),int(0)), (255,0,0), 2)
 
 	cv2.imshow('Three Lines', img)
 	cv2.waitKey(0)
@@ -167,7 +171,12 @@ def part5(image):
 	# vanish_line = part2(img)
 	mat = np.transpose(np.append(a, b, axis=1))
 	H = np.array([[1,0,0], [0,1,0]])
+	print(vanish_line)
+	# vanish_line /= np.sqrt(np.sum(vanish_line ** 2))
 	H = np.append(H, vanish_line.reshape(1, -1), axis=0)
+	print("Vanish Line - : ", vanish_line)
+	print("Rectifier Matrix - : ", H)
+	print("Normal Matrix - : ", mat)
 	final_H  = mat @ H
 	print(final_H.shape)
 	# for i in range(img.shape[0]):
@@ -188,13 +197,13 @@ if __name__ == '__main__':
 	# P2 = part1(img, 2)
 
 	# Part 2 : Vanishing Line
-	# line = part2(img)
+	line = part2(img)
 
 	# Part 3 : Parallel line to vanishing line
 	# part3(img)
 
 	# Part 4 : Three Lines
-	part4(img)
+	# part4(img)
 
 	# Part 5 : Affine rectification
-	# part5(img)
+	part5(img)
