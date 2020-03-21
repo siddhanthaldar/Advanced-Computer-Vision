f7b1b9ada72178dbc1bd6b016039c54a412d8f01
Harsh Maheshwari <harshmaheshwari135@gmail.com>
2020-03-21 15:28:49 +0530

    adding part 2

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -24,6 +24,36 @@ def part1(image1, image2):
 	# 	print(m.queryIdx, m.trainIdx)
 	# trainIdx -> keypoint 2
 
+# http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/MOHR_TRIGGS/node50.html -> for part 2
+def part2(image1, image2):
+	kp1, kp2, matches = part1(image1.copy(), image2.copy())
+	for i, match in enumerate(matches):
+		x = kp1[match.queryIdx].pt[0]
+		y = kp1[match.queryIdx].pt[1]
+		x_dash = kp2[match.trainIdx].pt[0]
+		y_dash = kp2[match.trainIdx].pt[0]
+		# print(str(x) + " " + str(y) + "    "+str(x_dash)+" "+str(y_dash))
+		a = [x*x_dash, x*y_dash, x, y*x_dash, y*y_dash, y, x_dash, y_dash, 1]
+		if (i == 0):
+			A = np.array(a).reshape(1,-1)
+		else:
+			A = np.append(A, np.array(a).reshape(1,-1), axis=0)
+	A_t = np.transpose(A)
+	product = A_t @ A
+	eigenvalue, eigenvector = np.linalg.eig(product)
+	f_rough = eigenvector[-1]
+	f_rough = f_rough.reshape(3, 3)
+	# SVD of f_rought
+	u, s, vh = np.linalg.svd(f_rough)
+	# making smallest singular vector 0
+	smat = np.zeros((s.shape[0], s.shape[0]), float)
+	smat[0][0] = s[0]
+	smat[1][1] = s[1]
+	
+	f = u @ smat @ vh
+	return f
+	print(f.shape, eigenvector.shape, product.shape, A_t.shape, A.shape, len(matches))
+
 if __name__ == '__main__':
 	
 	img1 = cv2.imread("Amitava_first.JPG", 0)
